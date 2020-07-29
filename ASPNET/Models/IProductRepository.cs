43b62086a5e4e86128f729ff3ec500b5c017f193
Jamey Geoghagan <Jameygeoghagan@gmail.com>
2020-07-28 20:36:01 -0500

    Added the product view page and the view item page

---
+++ ASPNET/Models/IProductRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASPNET.Models
+{
+    public interface IProductRepository
+    {
+        public IEnumerable<Product> GetAllProducts();
+        public Product GetProduct(int id);
+    }
+}
